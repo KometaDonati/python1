0f1ec72918dfe4693640d72a41371d026a3b9fc1
Rada <rada.donati@gmail.com>
2023-11-01 11:10:44 -0600

    start

---
+++ list.py
@@ -0,0 +1,47 @@
+#
+# l1 = [1,2,3,4,5,6]
+# l_names = ['Vadim', 'Igor', 'Anna']
+# print(l1)
+# print(l_names)
+#
+# l11 = []
+# l2 = list()
+# print(l11)
+# print(l2)
+#
+# obj_1 = [11,4,"Anna", [True, 23], {'Age',30}]
+# l22 = [5,6,4]
+# l23 = list(l22)
+#
+# l3 = [3]
+# l33 = l3 * 5
+# print(l33)
+#
+# l3 = [3]*5
+# l33 = l3
+# print(l33)
+#
+# l4 = ['inna', 'anna']
+# l44 = l4 * 5
+# print(l44)
+#
+# l4 = ['inna', 'anna', 'Serg']
+# l44_0 = l4[0]
+# l44_1 = l4[1]
+# l44_2 = l4[2]
+#
+# print(l44_0)
+#
+# l4 = ['inna', 'anna', 'Serg']
+# l4[1] = 'Ira'
+# print(l4)
+#
+# n1,n2,n3 = l4
+# print(n1)
+# print(n2)
+# print(n3)
+
+for i in 4:
+    print(i)
+
+while i <  len()
